c92cee06c3023c2ad506e1784b95dae918cb1c0b
Anh Le <kunquan2345@gmail.com>
2020-01-05 21:54:05 -0800

    Add reset form and reset the password
    - Because we use passport-local-mongoose, we cannot set/remove password manually
      (mean we can't just findAndUpdate() password - can't hash by themselves)
    - We need to use setPassword() method provide by passport-local-mongoose
      This one doesn't save the model we set password for, thus we need to save manually like user.save()

--- routes/index.js
+++ routes/index.js
@@ -93,36 +93,88 @@ router.get("/forgot", (req, res) => {
 });
 router.post("/forgot", async (req, res) => {
   try {
-    const foundUser = await User.findOne({
-      email: req.body.email
-    });
+    const token = crypto.randomBytes(50).toString("hex");
+
+    const foundUser = await User.findOneAndUpdate(
+      {
+        email: req.body.email
+      },
+      {
+        resetPasswordToken: token,
+        resetPasswordExpire: Date.now() + 15 * 60 * 1000
+      }
+    );
     if (!foundUser) {
       req.flash("error", "No account with that email address exists.");
       return res.redirect("/forgot");
     }
 
-    // Create user token
-    const token = crypto.randomBytes(50).toString("hex");
-    foundUser.resetPasswordToken = token;
-    foundUser.resetPasswordExpire = Date.now() + 3600000;
-    await foundUser.save();
-
     // Send email to user
-    const message = {
+    await sgMail.send({
       to: req.body.email,
       from: "nadom45981@mailon.ws",
       subject: "Node.js Password Reset",
       html: ` <p>You are receiving this because you (or someone else) have requested the reset of the password for your account.</p>
         <p>Please click on the following link, or paste this into your browser to complete the process: </p>
         <p>http://${req.headers.host}/reset/${token} </p>
+        <p>The link will expire in 15 minutes </p>
         <p>If you did not request this, please ignore this email and your password will remain unchanged. </p>`
-    };
-    await sgMail.send(message);
+    });
     // Display flash message if success
     req.flash("success", `An email has been sent to ${req.body.email}`);
-    res.redirect("/forgot");
+    return res.redirect("/forgot");
   } catch (err) {
-    console.log(err);
+    req.flash("error", "Something went wrong");
+    return res.redirect("/forgot");
+  }
+});
+
+// Show reset password form
+router.get("/reset/:token", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      resetPasswordToken: req.params.token,
+      resetPasswordExpire: {
+        $gt: Date.now()
+      }
+    });
+    if (!user) {
+      req.flash("error", "Password reset token is invalid or has expired.");
+      return res.redirect("/forgot");
+    }
+    res.render("users/reset", { token: req.params.token });
+  } catch (err) {
+    req.flash("error", "Something went wrong");
+    return res.redirect("/forgot");
+  }
+});
+router.post("/reset/:token", async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      {
+        resetPasswordToken: req.params.token,
+        resetPasswordExpire: {
+          $gt: Date.now()
+        }
+      },
+      {
+        resetPasswordToken: null,
+        resetPasswordExpire: null
+      },
+      { new: true }
+    );
+    if (!user) {
+      req.flash("error", "Password reset token is invalid or has expired.");
+      return res.redirect("/forgot");
+    }
+    if (req.body.password === req.body.confirm) {
+      await user.setPassword(req.body.password);
+      user.save();
+    } else {
+      res.redirect(`/reset/${req.params.token}`);
+    }
+    return res.redirect("/login");
+  } catch (err) {
     req.flash("error", "Something went wrong");
     return res.redirect("/forgot");
   }
