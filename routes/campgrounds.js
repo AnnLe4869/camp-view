13eb7d75f28b6e4128b4e8711594a68c51d5d11c
Anh Le <kunquan2345@gmail.com>
2020-01-07 15:32:31 -0800

    Install all necessary package for image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,7 +19,10 @@ router.get("/", async (req, res) => {
     const campgrounds = await Campground.find({});
     const searchOption = {
       shouldSort: true,
-      threshold: 0.2,
+      threshold: 0.6,
+      location: 0,
+      distance: 10,
+      maxPatternLength: 32,
       keys: ["name", "description", "location", "author.username"]
     };
     const fuse = new Fuse(campgrounds, searchOption);
