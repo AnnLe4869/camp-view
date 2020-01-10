17a159ff56b28c10f855d19e49e7c314658a1f7f
Anh Le <kunquan2345@gmail.com>
2020-01-10 03:17:03 -0800

    Styling a little bit better
    Need to modify more in CSS manually

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV) {
 }
 //const seedDB = require("./seed");
 const User = require("./models/user");
+const Notification = require("./models/notification");
 const authRoute = require("./routes/user/index");
 const commentRoute = require("./routes/comment/comments");
 const campgroundRoute = require("./routes/campground/campgrounds");
@@ -45,10 +46,52 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   res.locals.user = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
+  if (req.user) {
+    try {
+      const { comments, campgrounds, lastReadAt } = await Notification.findOne({
+        user: req.user._id
+      })
+        .populate({
+          path: "campgrounds",
+          select: "name author.username createdAt",
+          options: {
+            sort: {
+              createdAt: -1
+            }
+          }
+        })
+        .populate({
+          path: "comments",
+          options: {
+            sort: {
+              createdAt: -1
+            }
+          }
+        });
+      const allMessages = [
+        ...comments.map(comment => {
+          comment.type = "comment";
+          return comment;
+        }),
+        ...campgrounds.map(campground => {
+          campground.type = "campground";
+          return campground;
+        })
+      ].sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
+      res.locals.newMessages = allMessages.filter(
+        post => post.createdAt > lastReadAt
+      );
+      res.locals.oldMessages = allMessages.filter(
+        post => post.createdAt < lastReadAt
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return next();
 });
 passport.serializeUser(User.serializeUser());
---
+++ background-run/email-send/index.js
@@ -0,0 +1,68 @@
+async (req, res) => {
+    try {
+        const {
+            comments: [comment, ...restComments],
+            campgrounds: [campground, ...restCampgrounds],
+            lastReadAt
+        } = await Notification.findOne({
+            user: req.user._id
+        })
+            .populate({
+                path: "campgrounds",
+                select: "name author.username createdAt",
+                options: {
+                    sort: {
+                        createdAt: -1
+                    }
+                }
+            })
+            .populate({
+                path: "comments",
+                options: {
+                    sort: {
+                        createdAt: -1
+                    }
+                }
+            });
+        const message = (
+            // In case the comment or campground is undefined
+            comment = {
+                author: { username: "" },
+                text: "",
+                createdAt: 0
+            },
+            campground = {
+                author: { username: "" },
+                name: "",
+                createdAt: 0
+            }
+        ) => {
+            const returnCases = {
+                noNotification: {
+                    author: "",
+                    action: "No new notification",
+                    text: "",
+                    id: ""
+                },
+                newComment: {
+                    author: comment.author.username,
+                    action: "create a new comment",
+                    text: comment.text,
+                    id: comment._id
+                },
+                newCampground: {
+                    author: campground.author.username,
+                    action: "create new campground",
+                    text: campground.name,
+                    id: campground._id
+                }
+            };
+            // Check if the post created date newer than the last time notification visit
+            if (comment.createdAt > lastReadAt || campground.createdAt > lastReadAt) {
+                return comment.createdAt > campground.createdAt
+                    ? returnCases.newComment
+                    : returnCases.newCampground;
+            } else {
+                return returnCases.noNotification;
+            }
+        };
