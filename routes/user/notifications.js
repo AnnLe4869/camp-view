66039f0b604d72f0825810ffb579cc7d8772b238
Anh Le <kunquan2345@gmail.com>
2020-01-10 05:19:16 -0800

    Update the lastReadAt of the Notification when user click the button
    - Have to be AJAX call so that the page won't reload
    - Seem to work fine

--- routes/user/notifications.js
+++ routes/user/notifications.js
@@ -3,8 +3,19 @@ const Notification = require("../../models/notification");
 const User = require("../../models/user");
 const { isSignedIn } = require("../../middleware/index");
 
-// router.get("/notification", isSignedIn, async (req, res) => {
-//   res.render("notifications/index");
-// });
+router.get("/notification", isSignedIn, async (req, res) => {
+  try {
+    await Notification.findOneAndUpdate(
+      {
+        user: req.user._id
+      },
+      { lastReadAt: Date.now() }
+    );
+  } catch (err) {
+    console.log(err);
+    req.flash("error", "Something went wrong. Please try again");
+    res.redirect("/campgrounds");
+  }
+});
 
 module.exports = router;
