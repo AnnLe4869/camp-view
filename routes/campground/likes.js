bf54fa6b234604981dee35f7d1f32ad9e64f445d
Anh Le <kunquan2345@gmail.com>
2020-01-11 04:43:50 -0800

    Create like function
    - Intend to be AJAX call for update like list, but combine EJS and <script> tag
      is a bad idea. Thing went all wrong
    - Right now when like/unlike the page will reload

--- routes/campground/likes.js
+++ routes/campground/likes.js
@@ -10,21 +10,23 @@ router.post("/", isSignedIn, async (req, res) => {
     // Reject if user try to like himself
     if (currentCampground.author.id.toString() === req.user._id.toString()) {
       req.flash("err", "You cannot like your own campground");
-      console.log("No self promote");
       return res.redirect("/campgrounds");
     }
-    if (
+    // Check if user already like the campground
+    // If yes, unlike it
+    const isUserAlreadyLiked =
       currentCampground.likes.filter(likedUser => likedUser.equals(user._id))
-        .length > 0
-    ) {
+        .length > 0;
+    if (isUserAlreadyLiked) {
       await currentCampground.likes.pull(req.user._id);
       req.flash("success", "Remove from liked campgrounds");
     } else {
       currentCampground.likes.push(user);
       req.flash("success", "Added to liked campgrounds");
     }
+
     await currentCampground.save();
-    res.redirect("/campgrounds");
+    res.redirect(`/campgrounds/${req.params.id}`);
   } catch (err) {
     console.error(err);
     req.flash("err", "Something went wrong");
